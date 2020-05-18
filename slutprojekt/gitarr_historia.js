1a777b68ba97e1c09137b9e17c8c0a71bae83f77
Viktor Larsson <viktor.larsson8@learnet.se>
2020-05-18 10:51:00 +0200

    Försöker lägga in darkmode och lightmode knapp

--- slutprojekt/gitarr_historia.js
+++ slutprojekt/gitarr_historia.js
@@ -1,5 +1,35 @@
 const eFlyttandeElement = document.querySelectorAll(".skrolla"); 
-const eSökFält = document.querySelector(".sok_box input")
+
+function darkmode() {
+    const eBody = document.querySelector(".innehall");
+    const eBox = document.querySelector("body");
+    const eH1 = document.querySelector("h1");
+    const eH3 = document.querySelector("h3");
+    const eKnapp1 = document.querySelector("#K1");
+    const eKnapp2 = document.querySelector("#K2");
+
+    eBox.style.backgroundColor = "#FFFFFF";
+    eH1.style.color = "#444";
+    eH3.style.color = "#888";
+    eKnapp1.style.color = "#222";
+    eKnapp2.style.color = "#222";
+
+}
+function lightmode() {
+    const eBody = document.querySelector(".innehall");
+    const eBox = document.querySelector("body");
+    const eH1 = document.querySelector("h1");
+    const eH3 = document.querySelector("h3");
+    const eKnapp1 = document.querySelector("#K1");
+    const eKnapp2 = document.querySelector("#K2");
+
+    eBox.style.backgroundColor = "white";
+    eH1.style.color = "#FFF";
+    eH3.style.color = "#BBB";
+    eKnapp1.style.color = "#777";
+    eKnapp2.style.color = "#777";
+
+}
 
 window.addEventListener("skrolla", function (e) {
     
