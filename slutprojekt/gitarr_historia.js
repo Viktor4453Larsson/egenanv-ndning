8d6183f32f41e3d99cfe7c3a4a60742b8266714e
Viktor Larsson <viktor.larsson8@learnet.se>
2020-05-27 12:32:05 +0200

    Ersatt onclick med addeventlisener, är helt klar.

--- slutprojekt/gitarr_historia.js
+++ slutprojekt/gitarr_historia.js
@@ -1,43 +1,51 @@
-const eFlyttandeElement = document.querySelectorAll(".skrolla"); 
+const eFlyttandeElement = document.querySelectorAll(".skrolla");
+const elementTal = document.querySelector("input");
+const elementP = document.querySelector("p");
+const elementLabel = document.querySelector("label");
+const elementInput = document.querySelector("input");
+const eBody = document.querySelector(".innehall");
+const eBox = document.querySelector("body");
+const eH1 = document.querySelector("h1");
+const eH3 = document.querySelector("h3");
+const eSkicka = document.querySelector("#skicka");
+eSkicka.addEventListener("click", gissa);
 
-/* En knapp för att aktivera mörkt tema på sidan */
-function darkmode() {
-    const eBody = document.querySelector(".innehall");
-    const eBox = document.querySelector("body");
-    const eH1 = document.querySelector("h1");
-    const eH3 = document.querySelector("h3");
-    const eKnapp1 = document.querySelector("#K1");
-    const eKnapp2 = document.querySelector("#K2");
 
-    eBox.style.backgroundColor = "#001100";
-    eH1.style.color = "#FFFFFF";
-    eH3.style.color = "#FFFFFF";
-    eKnapp1.style.color = "#222";
-    eKnapp2.style.color = "#222";
-}
-/* En knapp för att aktivera ljust läge på sidan och att byta från mörkt tema till ljust tema */
-function lightmode() {
-    const eBody = document.querySelector(".innehall");
-    const eBox = document.querySelector("body");
-    const eH1 = document.querySelector("h1");
-    const eH3 = document.querySelector("h3");
-    const eKnapp1 = document.querySelector("#K1");
-    const eKnapp2 = document.querySelector("#K2");
-
-    eBox.style.backgroundColor = "#FFEEFF";
-    eH1.style.color = "#333";
-    eH3.style.color = "#444";
-    eKnapp1.style.color = "#777";
-    eKnapp2.style.color = "#777";
+/* En funktion som kan ändra tema på sidan */
+function gissa() {
+    var tema = elementInput.value
+    if (tema == "darkmode") {
+        /* Rätt svar! */
+        elementP.textContent = "Rätt Svar!";
+        eBox.style.backgroundColor = "#001100";
+        eH1.style.backgroundColor = "#FFFFFF";
+        eH3.style.backgroundColor = "#FFFFFF";
+        eKnapp1.style.backgroundColor = "#222";
+        eKnapp2.style.backgroundColor = "#222";
+    } else {
+        /* Fel Svar! */
+        elementP.textContent = "Testa temat ligtmode eller darkmode!"
+    }
+    /* Ett annat alternativt val fast med ljust tema */
+    if (tema == "lightmode") {
+        /* Rätt svar! */
+        eBox.style.backgroundColor = "#FFEEFF";
+        eH1.style.backgroundColor = "#333";
+        eH3.style.backgroundColor = "#444";
+        eKnapp1.style.backgroundColor = "#777";
+        eKnapp2.style.backgroundColor = "#777";
+    } else {
+        /* Fel Svar! */
+        elementP.textContent = "Testa temat ligtmode eller darkmode!";
+    }
 }
 /* Kod för att animera note.png bilden på sidan */
 window.addEventListener("scroll", function (e) {
-    
+
     eFlyttandeElement.forEach(function (element) {
-        
+
         var rate = element.dataset.rate;
-    
+
         element.style.transform = "translateY(" + window.pageYOffset * rate + "px)";
     });
 });
-
