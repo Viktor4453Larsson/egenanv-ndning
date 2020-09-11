aff1140b92115f37f8c6db1cfaa2308ff0208bb2
Viktor Larsson <viktor.larsson8@learnet.se>
2020-09-11 12:49:07 +0200

    new email funktion fixad

---
+++ email_med_bilder/email.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img src="./egenanv-ndning/bilder/bergtest1.jpg" alt="berg1">
+    <img src="./egenanv-ndning/bilder/bergtest2.jpg" alt="berg2">
+    <script>
+
+const imageNumberOne = new Image (225, 225);
+imageNumberOne.src = "picture.jpg";
+document.body.appendChild(imageNumberOne);
+
+</script>
+<?php
+
+$bild = ["bild1", "bild2"];
+
+
+if ( == true) {
+   echo "Bild 1 visas!";
+} else {
+   echo "Bild 1 visas inte!";
+}
+?>
+</body>
+</html>
---
+++ index3.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lära sig PHP del 3</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    switch(1){
+        case(1):
+            echo "fungerar"; 
+        break;
+        case(2):
+            echo "fungerar"; 
+        break;
+        case(3):
+            echo "fungerar"; 
+        break;
+        default: 
+        echo "failed"; 
+
+    }
+    ?>
+</body>
+</html>
