5be5d88cc25edcc2dfcc3802e91b2a53c1b0af5e
Viktor4453Larsson <viktor.larsson@elev.ga.ntig.se>
2021-02-23 14:50:34 +0100

    Fortsatt med en ny navigationsbar som projekt.

---
+++ JS_projekt/navigation/nav.js
@@ -0,0 +1,15 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navSkifftande = document.querySelector(".navigera_runt");
+const lankar = document.querySelector(".navigera_runt");
+
+navSkifftande.addEventListener("click", function () {
+   // console.log(lankar.classList);
+    //console.log(lankar.classList.contains("random"));
+    console.log(lankar.classList.contains("lankar"));
+
+});
